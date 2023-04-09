0fd1c8a7e858e5d31c29237cab15274102705ce5
Aleksey Kholmogorov <alexholm222@yandex.ru>
2023-04-09 16:43:35 +0300

    fix/в Api исправлен запрос с http на https

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,6 +1,6 @@
 
 export const getCardsHotel = async (city, checkIn, checkOut) => {
-    return fetch(`http://engine.hotellook.com/api/v2/cache.json?location=${city}&currency=rub&limit=50&checkIn=${checkIn}&checkOut=${checkOut}`)
+    return fetch(`https://engine.hotellook.com/api/v2/cache.json?location=${city}&currency=rub&limit=50&checkIn=${checkIn}&checkOut=${checkOut}`)
       .then(res => {
         if (res.ok) {
             return res.json()
